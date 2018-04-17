fb082a3af36ee2fff8f850d18c3d34af83e289b7
MiyagiTakumi <38201727+MiyagiTakumi@users.noreply.github.com>
2018-04-18 07:35:04 +0900

    0418

--- 02/Assignment_0416/risan.cpp
+++ 02/Assignment_0416/risan.cpp
@@ -1,53 +1,52 @@
-// Assignment_0416.cpp : アプリケーションのエントリ ポイントを定義します。
-//
-
 #include "stdafx.h"
+#include <string>
 
-
-int main() {
-	const int x_ini = 100;	//初期個体数
-	const float r = 4;	//Reproduction Rate
-	const float k = 30000;	//Carrying Capacity
-
-
-	const int Thr_Abs = 10;
-	const int Thr_Time = 100;
-	/*｛（次の世代の個体数）ー（今の世代の個体数）の絶対値｝＜　Thr_Abs
-	が　Thr_Time　回連続すると平衡状態と判定*/
-
+void simulate(const long x_ini, const float r, const long double k, const int MAX_GEN) {
 
 	int x_crnt = 0;	//今の世代の個体数
 	int x_next = 0;	//次の世代の個体数
-	int i = 2;	//世代数カウンター
-	int Eq_Count = 0;
+
 
 	x_crnt = x_ini;
 
-	while (true) {
+
+	std::string FILENAME;
+	FILENAME = "result_r=" + std::to_string(r) + ".txt";
+
+	const char* FILENAME_array = FILENAME.c_str();
+
+	FILE* fp;
+	errno_t eCode = fopen_s(&fp, FILENAME_array, "w");
+
+	fprintf(fp, "第  1世代\t%d匹\n", x_ini);
+
+	for (int i = 2; i < MAX_GEN; i++) {
+
 		x_next = int(x_crnt + r * (1 - x_crnt / k)*x_crnt);	//ロジスティックモデル
 
-		if (unsigned(x_next - x_crnt) < Thr_Abs)
-			Eq_Count++;
-		else
-			Eq_Count = 0;
+		fprintf(fp, "第%3d世代\t%d匹\n", i, x_next);
 
-		printf("第%d世代\t%d匹\n", i, x_next);
-		i++;
-
-		x_crnt = x_next;
-
-		if (Eq_Count == Thr_Time)
-			break;
-
-		if (x_crnt < 1) {
-			x_crnt = 0;
+		if (x_next < 1) {
 			break;
 		}
+	
+		x_crnt = x_next;
 	}
 
-	printf("初期個体数：%d\n増殖率：%f\n環境収容力：%f\n\n", x_ini, r, k);
-	printf("次の世代との個体数の差が%d以下である場合が%d世代連続したとき平衡状態と判定\n\n", Thr_Abs, Thr_Time);
-	printf("平衡状態での個体数：%f", x_crnt);
+	fclose(fp);
+
+}
+
+int main() {
+	const long x_ini = 100;	//第１世代個体数
+	const float MIN_r = 1.0;	//最低増殖率
+	const float MAX_r = 10;	//最大増殖率
+	const long double k = 30000;	//環境収容力
+	const int MAX_GEN = 300;	//最大世代数
+
+	for (float r = MIN_r; r < MAX_r; r += 0.1) {
+		simulate(x_ini, r, k, MAX_GEN);
+	}
 
 	return 0;
 }
