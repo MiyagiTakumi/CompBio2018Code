2d6db01e0343d1c8fdc3507bf80f23227b2e1093
MiyagiTakumi <38201727+MiyagiTakumi@users.noreply.github.com>
2018-04-23 14:21:56 +0900

    0423 source

---
+++ 03/ExpGrowth.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double x_crnt = 0;	//x(t)
+	double x_next = 0;	//x(t + Δt)
+
+	const double x_0 = 100;	//x(0) 
+
+	const float m = 2.0;	//マルサス係数
+
+	const double accuracy = pow(10, 7);
+	const double dt = 1 / accuracy;	//Δt
+
+	const double t = 50;	//求めたいx(t)のt
+
+
+	x_crnt = x_0;
+
+	for (long i = 0; i < t * accuracy; i++) {
+
+		x_next = x_crnt + dt * m * x_crnt;
+		
+		x_crnt = x_next;
+	}
+
+	cout << "x(" << t << ") = " << x_crnt << endl;
+
+	return 0;
+}
---
+++ 03/LogisticGroeth.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double x_crnt = 0;	//x(t)
+	double x_next = 0;	//x(t + Δt)
+
+	const double x_0 = 100;	//x(0) 
+
+	const float r = 2.0;	//増殖率
+	const float K = 10000;	//環境収容力
+
+	const double accuracy = pow(10, 7);	// 1/Δt
+	const double dt = 1 / accuracy;	//Δt
+
+	const double t = 50;	//求めたいx(t)のt
+
+
+	x_crnt = x_0;
+
+	for (long i = 0; i < t * accuracy; i++) {
+
+		x_next = x_crnt + dt * r * (1 - x_crnt / K) * x_crnt;
+
+		x_crnt = x_next;
+	}
+
+	cout << "x(" << t << ") = " << x_crnt << endl;
+
+	return 0;
+}
