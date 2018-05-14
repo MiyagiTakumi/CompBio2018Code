304b97534461e05977c3bb5567651e85879060f7
MiyagiTakumi <38201727+MiyagiTakumi@users.noreply.github.com>
2018-05-14 15:04:52 +0900

    0514

---
+++ 05/Dice_100.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+	unsigned int trial = 0;
+	unsigned int sum1 = 0, sum2 = 0;
+	unsigned int result[1000] = { 0 };
+	unsigned int elementnumber[5] = { 1,5,10,100,1000 };
+
+	srand(time(NULL));
+
+
+	for (int i = 0; i < 1000; i++) {
+		sum1 = 0;
+		trial = 0;
+
+		do {
+			sum1 += rand() % 6 + 1;
+			trial++;
+		} while (sum1 < 100);
+
+		result[i] = trial;
+	}
+
+
+	for (int i = 0; i < 5; i++) {
+
+		sum2 = 0;
+
+		for (int j = 0; j < elementnumber[i]; j++) {
+			sum2 += result[j];
+		}
+
+		cout << "average of " << elementnumber[i] << " data: " << float(sum2) / elementnumber[i] << endl;
+
+	}
+		return 0;
+}
---
+++ 05/RandomWalk_5line.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	int position = 0; 
+	int pos_log[100] = { 0 };
+
+	srand(time(NULL));
+	
+	fstream fout;
+	fout.open("RandomWalk.csv", ios::in);
+
+	for (int j = 0; j < 5; j++) {
+		position = 0;
+
+		for (int i = 1; i <= 99; i++) {
+			if (rand() % 2)
+				position++;
+			else
+				position--;
+
+			pos_log[i] = position;
+		}
+
+		for (int i = 0; i < 99; i++) {
+			fout << pos_log[i] << "," << flush;
+		}
+
+		fout << pos_log[99] << endl;
+
+	}
+
+	return 0;
+}
